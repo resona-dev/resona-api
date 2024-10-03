5ad802617f8a414ac5df7af52a3a69d4b3659c9b
jb3rndt <berndtjannis@gmail.com>
2024-10-03 14:41:40 +0200

    Adjust model to store timezone information

--- src/models.py
+++ src/models.py
@@ -1,14 +1,34 @@
-from sqlalchemy import Column, DateTime, PickleType, String, Unicode, UnicodeText
+from datetime import datetime
+from sqlalchemy import (
+    Column,
+    PickleType,
+    String,
+    TypeDecorator,
+    Unicode,
+    UnicodeText,
+)
 from src.database import Base
+
+
+class TimestampTZ(TypeDecorator):
+    impl = String
+
+    cache_ok = True
+
+    def process_bind_param(self, value: datetime, dialect):
+        return value.isoformat()
+
+    def process_result_value(self, value: str, dialect):
+        return datetime.fromisoformat(value)
 
 
 class CompletedJob(Base):
     __tablename__ = "completed_jobs"
 
     id = Column(Unicode(191), primary_key=True)
-    completed_at = Column(DateTime, nullable=False, primary_key=True)
+    completed_at = Column(TimestampTZ, nullable=False, primary_key=True)
     name = Column(UnicodeText)
-    created_at = Column(DateTime, nullable=False)
+    created_at = Column(TimestampTZ, nullable=False)
     status = Column(String, nullable=False)
     error_message = Column(String)
     trigger = Column(PickleType, nullable=False)
